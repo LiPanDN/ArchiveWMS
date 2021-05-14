d629a410ea5f1c84b66333edd05646fc80bf55df
beyond8848 <cplusplus@126.com>
2021-05-14 16:22:30 +0800

    Set up ArchiveWMS framework

---
+++ app/src/main/java/com/fuzheng/archivewms/AJGridItemActivity.java
@@ -0,0 +1,14 @@
+package com.fuzheng.archivewms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AJGridItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajgrid_item);
+    }
+}
